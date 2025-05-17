d4ec5a10ef966c0bed57084f4dabdcd131c8923a
Senyx <133978434+SenyxLois@users.noreply.github.com>
2025-05-17 19:04:24 +0000

    test

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -1,21 +1,51 @@
 package com.example
 
+import androidx.appcompat.app.AppCompatActivity
+import com.lagradost.cloudstream3.Episode
+import com.lagradost.cloudstream3.HomePageList
+import com.lagradost.cloudstream3.HomePageResponse
+import com.lagradost.cloudstream3.LoadResponse
 import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.MainPageRequest
 import com.lagradost.cloudstream3.SearchResponse
+import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.mainPageOf
+import com.lagradost.cloudstream3.newEpisode
+import com.lagradost.cloudstream3.newHomePageResponse
+import com.lagradost.cloudstream3.newMovieLoadResponse
+import com.lagradost.cloudstream3.newMovieSearchResponse
+import com.lagradost.cloudstream3.newTvSeriesLoadResponse
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.ExtractorLinkType
+import com.lagradost.cloudstream3.utils.newExtractorLink
 
-class ExampleProvider : MainAPI() { // All providers must be an instance of MainAPI
+class ExampleProvider(val plugin: TestPlugin) : MainAPI() { // all providers must be an intstance of MainAPI
     override var mainUrl = "https://example.com/" 
     override var name = "Example provider"
     override val supportedTypes = setOf(TvType.Movie)
-
     override var lang = "en"
-
-    // Enable this when your provider has a main page
     override val hasMainPage = true
 
-    // This function gets called when you search for something
+    override val mainPage = mainPageOf(
+        "group_movie/english" to "English Movies"
+    )
+
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
+        val url = "$mainUrl${request.data}/page/$page/"
+        val doc = app.get(url).document
+
+        val home = doc.select("div.display-item").mapNotNull { toResult(it) }
+
+        return newHomePageResponse(
+            HomePageList(request.name, home, isHorizontalImages = false),
+            hasNext = doc.selectFirst(".pagination .current") != null // crude check for pagination
+        )
+    }
+
+    // this function gets called when you search for something
     override suspend fun search(query: String): List<SearchResponse> {
-        return listOf()
+        return listOf<SearchResponse>()
     }
 }
